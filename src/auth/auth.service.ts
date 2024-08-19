dbf8abeee6c8be742faa7dc0b9afbe5637a0edac
Edgar Vergara <edgarvm13@hotmail.com>
2024-08-19 09:33:12 +0200

    🚧 create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    console.log(createUserDto);
-    return 'This action adds a new auth';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const newUser = new this.userModel(createUserDto);
+
+      // encrypt password
+
+      //save user
+
+      // generate jwt
+
+      return newUser.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists!`);
+      }
+
+      throw new InternalServerErrorException('Something terrible happened');
+    }
   }
 
   findAll() {
@@ -18,6 +42,7 @@ export class AuthService {
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
+    console.log(updateUserDto);
     return `This action updates a #${id} auth`;
   }
 
